7bff509733b288262bce6ada5c565f0380e86e13
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-01-26 11:22:44 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
-const test = '1';
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let timerId = null;
+const bodyColor = document.body.style;
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    bodyColor.backgroundColor = randomColor;
+  }, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.disabled = false;
+});
