9c28a63ae740596b50986aede2cc179cdecf51a4
NCatalin93 <catalin.nedelcu93@gmail.com>
2024-01-26 17:07:40 +0200

    add task2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,10 +2,10 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
-const setDate = document.querySelector('input#datetime-picker');
+const selector = document.querySelector('input#datetime-picker');
 const startButton = document.querySelector('button[data-start]');
 
-flatpickr(setDate, options);
+let setDate = new Date();
 
 const options = {
   enableTime: true,
@@ -13,7 +13,13 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    setDate = selectedDates[0];
+    if (setDate < new Date()) {
+      startButton.disabled = true;
+      Notiflix.Notify.failure('Please choose a date in the future');
+    } else {
+      startButton.disabled = false;
+    }
   },
 };
 
@@ -39,3 +45,35 @@ function convertMs(ms) {
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+flatpickr(selector, options);
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+startButton.addEventListener('click', () => {
+  const timeFrame = setInterval(() => {
+    const remainingTime = setDate.getTime() - new Date().getTime();
+    if (remainingTime <= 0) {
+      clearInterval(timeFrame);
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(remainingTime);
+      document.querySelector('[data-days]').textContent = addLeadingZero(
+        days,
+        2
+      );
+      document.querySelector('[data-hours]').textContent = addLeadingZero(
+        hours,
+        2
+      );
+      document.querySelector('[data-minutes]').textContent = addLeadingZero(
+        minutes,
+        2
+      );
+      document.querySelector('[data-seconds]').textContent = addLeadingZero(
+        seconds,
+        2
+      );
+    }
+  }, 1000);
+});
