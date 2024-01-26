7103c3869c5d68200085d37e416a17ba28eaf901
NCatalin93 <142240336+NCatalin93@users.noreply.github.com>
2024-01-26 09:23:05 +0000

    Deploying to gh-pages from @ NCatalin93/goit-js-hw-09@7bff509733b288262bce6ada5c565f0380e86e13 🚀

---
+++ 01-color-switcher.598c7ee4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let o=null;const a=document.body.style;t.addEventListener("click",(()=>{o=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;a.backgroundColor=t}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(o),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.598c7ee4.js.map
